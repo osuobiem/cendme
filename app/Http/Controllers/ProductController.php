58b63ad720b9550f77bf30cdbf98013f40fb2f88
Gabriel Osuobiem <osuobiem@gmail.com>
2020-05-31 18:46:37 +0100

    Product update 90% done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
+    // CREATE PRODUCT
     /**
      * Create product
      * @return json
@@ -19,7 +20,7 @@ class ProductController extends Controller
     public function create(Request $request, $vendor_id)
     {
         // Get validation rules
-        $validate = $this->create_rules($request);
+        $validate = $this->rules($request);
 
         // Run validation
         if ($validate->fails()) {
@@ -62,11 +63,13 @@ class ProductController extends Controller
         $product->quantity = $request['quantity'];
         $product->vendor_id = $request['vendor_id'];
 
+        $stored = false;
+
         // Check for images
         if ($request['photo']) {
             $photo = $request['photo'];
-            $stored = Storage::disk('local')->put('/products', $photo);
-            $product->photo = basename($stored);
+            $stored = Storage::put('/products', $photo);
+            $product->photo = $stored ? basename($stored) : 'placeholder.png';
         }
 
         // Try vendor save or catch error if any
@@ -76,6 +79,10 @@ class ProductController extends Controller
             return ['success' => true, 'status' => 200, 'message' => 'Creation Successful'];
         } catch (\Throwable $th) {
             Log::error($th);
+
+            // Delete uploaded file if there's an error
+            $stored && $product->photo != 'placeholder' ? Storage::delete('/products/' . $product->photo) : '';
+
             return ['success' => false, 'status' => 500, 'message' => 'Internal Server Error'];
         }
     }
@@ -84,7 +91,7 @@ class ProductController extends Controller
      * Vendor Creation Validation Rules
      * @return object The validator object
      */
-    private function create_rules(Request $request)
+    private function rules(Request $request)
     {
         // Make and return validation rules
         return Validator::make($request->all(), [
@@ -94,4 +101,85 @@ class ProductController extends Controller
             'photo' => 'image|max:3072'
         ]);
     }
+    // -------------
+
+
+    // UPDATE PRODUCT
+    /**
+     * Update product data
+     * @param int $id Product id to update with
+     * @return json
+     */
+    public function update(Request $request, $id)
+    {
+        // Get validation rules
+        $validate = $this->rules($request);
+
+        // Run validation
+        if ($validate->fails()) {
+            return response()->json([
+                "success" => false,
+                "message" => $validate->errors()
+            ], 400);
+        }
+
+        // Store product data
+        $store = $this->ustore($request, $id);
+        $status = $store['status'];
+        unset($store['status']);
+        return response()->json($store, $status);
+    }
+
+    /**
+     * Process product data update
+     * @param int $id Product id to update with
+     * @return array Update status
+     */
+    public function ustore(Request $request, $id)
+    {
+        // Decode product id
+        $id = base64_decode($id);
+
+        // Find product with supplied id
+        $product = Product::find($id);
+
+        if ($product) {
+
+            // Assign product object properties
+            $product->title = $request['title'];
+            $product->details = $request['details'];
+            $product->quantity = $request['quantity'];
+
+            $old_photo = $product->photo;
+            $stored = false;
+
+            // Check for images
+            if ($request['photo']) {
+                $photo = $request['photo'];
+                $stored = Storage::put('/products', $photo);
+
+                $product->photo = $stored ? basename($stored) : '';
+            }
+
+            // Try vendor save or catch error if any
+            try {
+                $product->save();
+
+                // Delete previous photo
+                $stored && $old_photo != 'placeholder' ? Storage::delete('/products/' . $product->photo) : '';
+
+                return ['success' => true, 'status' => 200, 'message' => 'Update Successful'];
+            } catch (\Throwable $th) {
+                Log::error($th);
+
+                // Delete uploaded file if there's an error
+                $stored && $product->photo != 'placeholder' ? Storage::delete('/products/' . $product->photo) : '';
+
+                return ['success' => false, 'status' => 500, 'message' => 'Internal Server Error'];
+            }
+        } else {
+            return ['success' => false, 'status' => 404, 'message' => 'No product exists with this ID'];
+        }
+    }
+    // -------------
 }
--- database/migrations/2020_05_31_142243_products.php
+++ database/migrations/2020_05_31_142243_products.php
@@ -16,7 +16,7 @@ class Products extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('details');
+            $table->text('details');
             $table->integer('quantity');
             $table->json('other_details')->nullable();
             $table->string('photo')->default('placeholder.png');
