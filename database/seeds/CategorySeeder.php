25e6835ecc20f199fa76c3a52ff0e0347ffe615d
Gabriel Osuobiem <osuobiem@gmail.com>
2020-06-06 23:13:47 +0100

    CategorySeeder added

---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::create([
+            'name' => 'Food'
+        ]);
+        Category::create([
+            'name' => 'Drinks'
+        ]);
+        Category::create([
+            'name' => 'Health & Beauty'
+        ]);
+        Category::create([
+            'name' => 'Food'
+        ]);
+        Category::create([
+            'name' => 'Household/Cleaning'
+        ]);
+        Category::create([
+            'name' => 'Baby Products'
+        ]);
+        Category::create([
+            'name' => 'Plastic & Paper Products'
+        ]);
+        Category::create([
+            'name' => 'Miscellaneous'
+        ]);
+    }
+}
