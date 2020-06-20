c37ad25bcee26764c7fa2b54df56fb407b2092b5
Gabriel Osuobiem <osuobiem@gmail.com>
2020-06-20 23:20:04 +0100

    Added Order model

---
+++ app/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+    use SoftDeletes;
+
+
+    // Relationship with Product
+    public function product()
+    {
+        return $this->belongsToMany('App\Product')->withPivot('quantity');
+    }
+}
