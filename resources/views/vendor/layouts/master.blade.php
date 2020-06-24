85a61960bc42cc3d2a01d7dd89e978a95badfb67
Gabriel Osuobiem <osuobiem@gmail.com>
2020-06-24 12:33:45 +0100

    Vendor login page and process done

--- resources/views/vendor/layouts/master.blade.php
+++ resources/views/vendor/layouts/master.blade.php
@@ -10,10 +10,12 @@
   <title>@yield('title') | Cendme</title>
   <link href="{{url('assets/css/styles.css')}}" rel="stylesheet">
   <link href="{{url('assets/css/admin-style.css')}}" rel="stylesheet">
+  <link href="{{url('assets/css/custom.css')}}" rel="stylesheet">
 
   <!-- Vendor Stylesheets -->
   <link href="{{url('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{url('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet">
+  <link href="{{url('assets/css/animate.min.css')}}" rel="stylesheet">
 
   <script src="{{ url('assets/js/jquery-3.4.1.min.js') }}"></script>
 
@@ -23,13 +25,20 @@
 
 @yield('content')
 
+<div class="alert alert-success top-alert d-none" role="alert" id="success-alert">
+</div>
+
+<div class="alert alert-danger top-alert d-none" role="alert" id="error-alert">
+</div>
+
 <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ url('assets/vendor/chart/highcharts.js') }}"></script>
-<script src="{{ url('assets/vendor/chart/exporting.js') }}"></script>
-<script src="{{ url('assets/vendor/chart/export-data.js') }}"></script>
-<script src="{{ url('assets/vendor/chart/accessibility.js') }}"></script>
+<!-- <script src="{{ url('assets/vendor/chart/highcharts.js') }}"></script> -->
+<!-- <script src="{{ url('assets/vendor/chart/exporting.js') }}"></script> -->
+<!-- <script src="{{ url('assets/vendor/chart/export-data.js') }}"></script> -->
+<!-- <script src="{{ url('assets/vendor/chart/accessibility.js') }}"></script> -->
 <script src="{{ url('assets/js/scripts.js') }}"></script>
-<script src="{{ url('assets/js/chart.js') }}"></script>
+<script src="{{ url('assets/js/custom.js') }}"></script>
+<!-- <script src="{{ url('assets/js/chart.js') }}"></script> -->
 </body>
 
 </html>
--- resources/views/vendor/login.blade.php
+++ resources/views/vendor/login.blade.php
@@ -15,33 +15,45 @@
             <div class="col-xl-5 col-lg-6">
               <div class="card shadow-lg border-0 rounded-lg mt-5">
                 <div class="card-header card-sign-header">
-                  <h3 class="text-center font-weight-light my-4">Login</h3>
+                  <h3 class="text-center font-weight-light mt-4">Login</h3>
                 </div>
                 <div class="card-body">
-                  <form>
+                  <div class="alert alert-danger text-center" style="display: none;" role="alert" id="form-error"></div>
+                  <div class="alert alert-success text-center" style="display: none;" role="alert" id="form-success">
+                  </div>
 
+                  <form id="login-form" method="POST">
+                    @csrf
                     <div class="form-group">
                       <label class="form-label" for="inputEmailAddress">Email <span class="text-danger">*</span></label>
-                      <input class="form-control py-3" id="inputEmailAddress" type="email"
+                      <input class="form-control py-3" id="inputEmailAddress" name="email" type="email" required
                         placeholder="Enter email address">
-                      <span class="text-danger" id="email"></span>
+                      <span class="text-danger error-message" id="email"></span>
                     </div>
 
                     <div class="form-group">
                       <label class="form-label" for="inputPassword">Password <span class="text-danger">*</span></label>
-                      <input class="form-control py-3" id="inputPassword" type="password" placeholder="Enter password">
-                      <span class="text-danger" id="password"></span>
+                      <input class="form-control py-3" name="password" id="inputPassword" required type="password"
+                        placeholder="Enter password">
+                      <span class="text-danger error-message" id="password"></span>
                     </div>
 
                     <div class="form-group">
                       <div class="custom-control custom-checkbox">
-                        <input class="custom-control-input" id="rememberPasswordCheck" type="checkbox" checked />
+                        <input class="custom-control-input" id="rememberPasswordCheck" name="remember_me"
+                          type="checkbox" checked />
                         <label class="custom-control-label" for="rememberPasswordCheck">Remember me</label>
                       </div>
                     </div>
 
                     <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-                      <button class="btn btn-sign hover-btn" id="login">Login</button>
+                      <button class="btn btn-sign hover-btn" type="submit" id="login">
+                        <span id="btn-txt">Login</span>
+                        <div id="spinner" style="display: none;" class="spinner-border spinner-border-sm text-light"
+                          role="status">
+                          <span class="sr-only">Processing...</span>
+                        </div>
+                      </button>
                     </div>
                   </form>
                 </div>
@@ -52,4 +64,81 @@
       </main>
     </div>
   </div>
+
+  <script>
+
+    $('#login-form').submit(e => {
+      e.preventDefault();
+
+      offError();
+
+      let data = new FormData(e.target)
+      let url = "{{ url('vendor/p-login') }}"
+
+      spin()
+
+      $.ajax({
+        type: "POST",
+        url,
+        data,
+        processData: false,
+        contentType: false,
+      })
+        .then(res => {
+          spin()
+          success()
+          $('#form-success').text(res.message)
+
+          setTimeout(() => {
+            location.href = "{{ url('vendor') }}";
+          }, 1000)
+        })
+        .catch(err => {
+          spin()
+
+          if (err.status === 400) {
+            errors = err.responseJSON.message;
+
+            if (typeof errors === "object") {
+              for (const [key, value] of Object.entries(errors)) {
+                $('#' + key).html('');
+                [...value].forEach(m => {
+                  $('#' + key).append(`<p>${m}</p>`)
+                })
+              }
+            }
+            else {
+              error()
+              $('#form-error').text(errors)
+            }
+          }
+
+          else {
+            error()
+            $('#form-error').text("Oops! Something's not right. Try Again")
+          }
+        })
+    })
+
+    function spin() {
+      $('#btn-txt').toggle()
+      $('#spinner').toggle()
+    }
+
+    function error() {
+      $('#form-error').addClass('animate__animated animate__headShake')
+      $('#form-error').removeAttr('style');
+    }
+
+    function success() {
+      $('#form-success').addClass('animate__animated animate__fadeIn')
+      $('#form-success').removeAttr('style');
+    }
+
+    function offError() {
+      $('#form-error').attr('style', 'display: none');
+      $('.error-message').html('')
+    }
+  </script>
+
   @endsection
