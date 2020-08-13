e2237f6ac5efe29f2ccd866862861e0cbcfe01d5
Gabriel Osuobiem <osuobiem@gmail.com>
2020-08-14 00:28:31 +0100

    Added transactions migration

---
+++ database/migrations/2020_08_14_001602_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference');
+            $table->string('amount');
+            $table->integer('status')->default(0);
+            $table->foreignId('order_id')->constrained()->onUpdate('no action')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
